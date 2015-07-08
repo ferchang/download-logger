39070d3f73c711139c345acfbddc2d5ea0e49f7e
unknown <hm@hm-28590e26d063.(none)>
2015-07-08 13:29:45 +0330

    first

---
+++ download_logging/download.php
@@ -0,0 +1,125 @@
+<?php
+
+//--------- config ----------->
+
+error_reporting(E_ALL);
+ini_set('log_errors', 1);
+ini_set('error_log', 'error_log.txt');
+
+$min_duration=5;
+
+$chunk_size=1024;
+
+set_time_limit(60*30);
+
+//--------- config -----------<
+
+ini_set('display_errors', '0');
+
+ignore_user_abort(true);
+
+$t1=time();
+
+$path_parts=pathinfo($_GET['file']);
+$file_name=$path_parts['basename'];
+$file='../files/'.$file_name;
+if(!file_exists($file)) exit('404');
+$size=filesize($file);
+
+header('X-download-logger: true');
+header('Content-Type: application/octet-stream');
+header("Content-Disposition: attachment; filename=\"$file_name\"");
+header("Pragma: public");
+header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+header("Content-Length: $size");
+
+ob_flush();
+flush();
+
+$fp=fopen($file, 'rb');
+
+$output_bytes=0;
+
+while(!feof($fp)) {
+	$out=fread($fp, $chunk_size);
+	if(feof($fp)) {
+		$duration=time()-$t1;
+		$delta=$min_duration-$duration;
+		if($delta<0) $delta=0;
+		$tmp=substr($out, 0, strlen($out)-$delta);
+		echo $tmp;
+		ob_flush();
+		flush();
+		if(connection_aborted()) break;
+		$output_bytes+=strlen($tmp);
+		$duration=time()-$t1;
+		for($i=0; $i<$delta; $i++) {
+			sleep(1);
+			echo substr($out, $i-$delta, 1);
+			ob_flush();
+			flush();
+			if(connection_aborted()) break;
+			$output_bytes++;
+		}
+	}
+	else {
+		echo $out;
+		ob_flush();
+		flush();
+		if(connection_aborted()) break;
+		$output_bytes+=strlen($out);
+	}
+}
+fclose($fp);
+
+$total_duration=time()-$t1;
+if(!isset($duration)) $duration=$total_duration;
+
+if(!$duration) {
+	$speed='NA';
+	$speed_unit='';
+}
+else {
+	$speed=($output_bytes/$duration)*8;
+	if($speed>1000*1000) {
+		$speed/=1000*1000;
+		$speed_unit='Mbps';
+	}
+	else if($speed>1000) {
+		$speed/=1000;
+		$speed_unit='Kbps';
+	}
+	$speed=round($speed, 2);
+}
+
+$size2=$size;
+if($size2>1000*1000) {
+	$size2/=1000*1000;
+	$size_unit='MB';
+}
+else if($size2>1000) {
+	$size2/=1000;
+	$size_unit='KB';
+}
+else $size_unit='Bytes';
+$size2=round($size2, 2);
+
+$report="time: ".time();
+$report.="\nmethod: {$_SERVER['REQUEST_METHOD']}";
+$report.="\nfile: $file_name";
+$report.="\nsize: $size2 $size_unit";
+$report.="\nduration: $duration";
+$report.="\ntotal duration: $total_duration";
+$report.="\nspeed: $speed $speed_unit";
+$report.="\nip: {$_SERVER['REMOTE_ADDR']}";
+$report.="\nuser agent: {$_SERVER['HTTP_USER_AGENT']}";
+$report.="\noutput_bytes: $output_bytes";
+if(connection_aborted()) $report.="\naborted at ".round(($output_bytes/$size)*100, 2)."% ($output_bytes/$size)";
+$report.="\n\n";
+
+$fp=fopen('download_log.txt', 'a');
+flock($fp, LOCK_EX);
+fwrite($fp, $report);
+fclose($fp);
+
+?>
---
+++ test.php
@@ -0,0 +1,63 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+set_time_limit(60*2);
+
+header('Content-Type: text/html');
+
+$host='';
+$file='cryptopp522mingw-bin-nodebug.zip';
+$method='GET';
+$bytes=0;
+
+if(!$method) $method='GET';
+if(!$host) $host='localhost';
+
+echo '<pre>';
+echo "host: $host\nfile: $file\nmethod: $method\nbytes to get: $bytes\n";
+for($i=0; $i<500; $i++) echo '<!-- -->';
+ob_flush();
+flush();
+
+$service_port = 80;
+
+$address = gethostbyname($host);
+
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+
+$result = socket_connect($socket, $address, $service_port);
+
+$in = "$method /files/$file HTTP/1.1\r\n";
+$in .= "Host: $host\r\n";
+$in .= "User-Agent: my bot!\r\n";
+$in .= "Connection: Close\r\n\r\n";
+$out = '';
+
+socket_write($socket, $in, strlen($in));
+
+echo "Reading response: ";
+
+$fp=fopen('out.txt', 'wb');
+
+$out_bytes=0;
+while ($out = socket_read($socket, 1000)) {
+	//usleep(20000);
+	$len=strlen($out);
+	fwrite($fp, $out);
+    echo "$len-";
+	ob_flush();
+	flush();
+	if($bytes) {
+		$out_bytes+=$len;
+		if($out_bytes>=$bytes) {
+			socket_close($socket);
+			exit;
+		}
+	}
+}
+
+socket_close($socket);
+
+?>
