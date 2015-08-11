d7e33966afbcee8bdeac1f9963138c51e138264f
unknown <hm@hm-28590e26d063.(none)>
2015-08-11 23:14:15 +0330

    backup10

--- download_logging/include/code_log_common.php
+++ download_logging/include/code_log_common.php
@@ -6,9 +6,13 @@ $report="time: $t1";
 $report.="\nmethod: {$_SERVER['REQUEST_METHOD']}";
 $report.="\nfile: $file_name";
 if(!file_exists($file)) $report.=" (404 Not Found)";
+
 $report.="\nip: {$_SERVER['REMOTE_ADDR']}";
 if(isset($_SERVER['HTTP_CLIENT_IP'])) $report.="\nHTTP_CLIENT_IP: {$_SERVER['HTTP_CLIENT_IP']}";
 if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) $report.="\nHTTP_X_FORWARDED_FOR: {$_SERVER['HTTP_X_FORWARDED_FOR']}";
+if(isset($_SERVER['HTTP_X_FORWARDED'])) $report.="\nHTTP_X_FORWARDED: {$_SERVER['HTTP_X_FORWARDED']}";
+if(isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) $report.="\nHTTP_X_CLUSTER_CLIENT_IP: {$_SERVER['HTTP_X_CLUSTER_CLIENT_IP']}";
+
 $report.="\nuser agent: ";
 if(isset($_SERVER['HTTP_USER_AGENT'])) $report.=$_SERVER['HTTP_USER_AGENT'];
 else $report.="NA";
