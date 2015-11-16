90207ad9d54b25614d9ee5afe5bd1d65fff39dcb
ferchange <hamidreza_mz@yahoo.com>
2015-11-16 13:11:28 +0330

    backup11

--- download_logging/include/code_log_common.php
+++ download_logging/include/code_log_common.php
@@ -13,6 +13,13 @@ if(isset($_SERVER['HTTP_X_FORWARDED_FOR'])) $report.="\nHTTP_X_FORWARDED_FOR: {$
 if(isset($_SERVER['HTTP_X_FORWARDED'])) $report.="\nHTTP_X_FORWARDED: {$_SERVER['HTTP_X_FORWARDED']}";
 if(isset($_SERVER['HTTP_X_CLUSTER_CLIENT_IP'])) $report.="\nHTTP_X_CLUSTER_CLIENT_IP: {$_SERVER['HTTP_X_CLUSTER_CLIENT_IP']}";
 
+if(isset($_SERVER['HTTP_REFERER'])) {
+ if($_SERVER['HTTP_REFERER']!=='') $referrer=$_SERVER['HTTP_REFERER'];
+ else $referrer='Empty';
+}
+else $referrer='NA';
+$report.="\nreferrer: $referrer";
+
 $report.="\nuser agent: ";
 if(isset($_SERVER['HTTP_USER_AGENT'])) $report.=$_SERVER['HTTP_USER_AGENT'];
 else $report.="NA";
